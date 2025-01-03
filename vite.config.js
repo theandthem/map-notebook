69dfdf5a6164f9bda78ae18c5115353a498c357a
Jim Gowen <james.gowen@gomolearning.com>
2025-01-03 15:36:18 +0000

    Fix icons and add gh deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
+  base: "/map-notebook",
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(fileURLToPath(new URL('.', import.meta.url)), "./src"),
     },
   },
 })
